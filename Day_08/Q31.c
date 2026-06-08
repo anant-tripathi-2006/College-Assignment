9ca68b45c61758ae01427c7f9619289d04ff0da2
anant-tripathi-2006 <anantji1947@gmail.com>
2026-06-08 18:53:35 +0530

    my readme

---
+++ Day_08/Q31.c
@@ -0,0 +1,18 @@
+/*
+A
+A B
+A B C 
+A B C D 
+A B C D E
+*/
+#include<stdio.h>
+int main (){
+    for(int i=65;i<=69;i++){
+        for(int j=65;j<=i;j++){
+            printf("%c ",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
