4f923b3c2e66febb2e4a714cc33060f0987b2c08
anant-tripathi-2006 <anantji1947@gmail.com>
2026-06-13 20:28:26 +0530

    my readme

---
+++ Day_10/Q40.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    char ch;
+
+    for(i = 1; i <= 5; i++) {
+
+        for(j = 1; j <= 5 - i; j++) {
+            printf(" ");
+        }
+
+        ch = 'A';
+
+        for(j = 1; j <= i; j++) {
+            printf("%c", ch++);
+        }
+
+        ch -= 2;
+
+        for(j = 1; j < i; j++) {
+            printf("%c", ch--);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
